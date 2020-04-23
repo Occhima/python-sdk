ed599c26e06a948406cb3ac7472f4722e0db70b1
Poseyy <lpose22@gmail.com>
2020-04-23 12:14:21 -0400

    push

--- spawner/nlp.py
+++ spawner/nlp.py
@@ -5,15 +5,13 @@ pd.options.display.width = 0
 # return answer as dataframe 
 def answer(question): 
     try: 
-        url = "http://0.0.0.0:5000/answer"
+        url = "https://spawnerapi.com/answer"
         trade_list = {"text": question}
         headers={'Content-type':'application/json'}
         r = requests.post(url, headers=headers, data=json.dumps(trade_list))
         content = r.text
         df = pd.read_json(content, orient='records')
-        df = df.drop(columns=['chart_type', 'condensed_data'])
+        df = df.drop(columns=['chart_type', 'condensed_data', 'data'])
         return df
     except: 
         return 'I am not smart enough to answer that question (yet). I am currently learning how to understand economics and almost have it figured out... Please try again.'
-
-print(answer('p/e ratio of apple?'))
